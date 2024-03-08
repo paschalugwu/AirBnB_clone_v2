292dc99928ee66c898712032eb29931da6ec58e1
paschalugwu <ugwupaschal@gmail.com>
2024-03-08 11:14:18 +0100

    feat(setup): Add script to set up web servers for web_static deployment
    Added a bash script that sets up web servers for the deployment of web_static. The script installs Nginx if not already installed, creates necessary directories, generates a fake HTML file, creates a symbolic link, assigns ownership recursively, updates Nginx configuration, and restarts Nginx service.

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,31 +1,18 @@
 #!/usr/bin/env bash
-# This script sets up web servers for the deployment of web_static.
-
-# Install Nginx if not already installed
-sudo apt-get update
+# sets up my web servers for the deployment of web_static
+sudo apt -y update
 sudo apt-get -y install nginx
-
-# Create necessary directories
-sudo mkdir -p /data/web_static/releases/test/
-sudo mkdir -p /data/web_static/shared/
-
-# Create a fake HTML file
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
-
-# Create symbolic link
+sudo ufw allow 'Nginx HTTP'
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<h1>Test Page</h1>" > /data/web_static/releases/test/index.html
+if [ -d "/data/web_static/current" ];
+then
+    echo "path /data/web_static/current exists"
+    sudo rm -rf /data/web_static/current;
+fi;
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-
-# Assign ownership
-sudo chown -R $USER:$USER /data/
-
-# Update Nginx configuration
-sudo sed -i '/^\s*location \/hbnb_static {/a \\\talias /data/web_static/current/;' /etc/nginx/sites-available/default
-
-# Restart Nginx
+sudo chown -hR ubuntu:ubuntu /data
+#sed -i "/server_name _;/a \\\n\tlocation /hbnb_static { \n\t\talias /data/web_static/current/;\n\t\tautoindex on;\n\t}" /etc/nginx/sites-available/default
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
 sudo service nginx restart
