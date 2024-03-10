1b8032b83ea0c6f6805b21ac5aacf62f800793fa
paschalugwu <ugwupaschal@gmail.com>
2024-03-10 14:14:13 +0100

    feat(do_deploy): Add Fabric script for deploying web archives
    Add a Fabric script named do_deploy to deploy archives to web servers. The script uploads the archive to the server, extracts its contents, and updates the symbolic link to the current release. It also removes obsolete directories and files after deployment.
    
    The script checks for the existence of the archive file, uploads it to the server, extracts its contents, and performs cleanup tasks to ensure a successful deployment.

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,87 +1,33 @@
 #!/usr/bin/python3
-'''Fabric script that deploys an archive to web servers'''
-from fabric.api import local, put, run, env
-from fabric.decorators import runs_once
-from datetime import datetime
-import re
-from os import path
+"""
+Fabric script (based on the file 1-pack_web_static.py) that distributes an
+archive to your web servers, using the function do_deploy
+"""
+from fabric.api import put, run, env
+from os.path import exists
 
-# Define the list of web servers
-env.hosts = [
-    '54.161.251.141',
-    '54.237.108.159'
-]
+env.hosts = ['54.161.251.141', '54.237.108.159']
 
 
 def do_deploy(archive_path):
-    '''Distribute an archive to web servers'''
-    # Check if the archive file exists
-    if not path.exists(archive_path):
-        return False
-
-    # Extract the filename from the archive path
-    file_name = re.search(r'versions/(\S+).tgz', archive_path)
-    if file_name is None:
-        return False
-    file_name = file_name.group(1)
-
-    # Upload the archive to the remote /tmp directory
-    res = put(local_path=archive_path, remote_path="/tmp/{}.tgz"
-              .format(file_name))
-    if res.failed:
-        return False
-
-    # Create a directory to extract the archive contents on the server
-    res = run("mkdir -p /data/web_static/releases/{}".format(file_name))
-    if res.failed:
-        return False
-
-    # Extract the archive contents to the specified directory
-    res = run("tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/"
-              .format(file_name, file_name))
-    if res.failed:
-        return False
-
-    # Remove the uploaded archive file from the server
-    res = run('rm -rf /tmp/{}.tgz'.format(file_name))
-    if res.failed:
-        return False
-
-    # Delete the existing directories if they exist
-    res = run('rm -rf /data/web_static/releases/{}/images'
-              .format(file_name))
-    if res.failed:
-        return False
-
-    res = run('rm -rf /data/web_static/releases/{}/styles'
-              .format(file_name))
-    if res.failed:
-        return False
-
-    # Now you can move the new directories
-    res = run(('mv /data/web_static/releases/{}/web_static/* ' +
-              '/data/web_static/releases/{}/')
-              .format(file_name, file_name))
-    if res.failed:
-        return False
-
-    # Remove the obsolete web_static directory
-    res = run('rm -rf /data/web_static/releases/{}/web_static'
-              .format(file_name))
-    if res.failed:
-        return False
-
-    # Remove the symbolic link to the current release
-    res = run('rm -rf /data/web_static/current')
-    if res.failed:
-        return False
-
-    # Create a new symbolic link to the current release
-    res = run('ln -s /data/web_static/releases/{}/ /data/web_static/current'
-              .format(file_name))
-    if res.failed:
-        return False
-
-    # Print confirmation message if deployment was successful
-    print('New version deployed!')
-    return True
+    """Distributes an archive to your web servers"""
+    if exists(archive_path):
+        try:
+            put(archive_path, "/tmp/")
+            file_name = archive_path.split("/")[-1]
+            no_ext = file_name.split(".")[0]
+            run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+                format(file_name, no_ext))
+            run("rm /tmp/{}".format(file_name))
+            run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(no_ext, no_ext))
+            run("rm -rf /data/web_static/releases/{}/web_static".
+                format(no_ext))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+                format(no_ext))
+            return True
+        except Exception as e:
+            print("Exception occurred: {}".format(e))
+    return False
--- fabfile.py
+++ fabfile.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-# Fabfile to delete out-of-date archives.
-import os
-from fabric.api import *
+"""
+Fabric script (based on the file 1-pack_web_static.py) that distributes an
+archive to your web servers, using the function do_deploy
+"""
+from fabric.api import put, run, env
+from os.path import exists
 
-env.hosts = ["54.161.251.141", "54.237.108.159"]
+env.hosts = ['54.161.251.141', '54.237.108.159']
 
 
-def do_clean(number=0):
-    """Delete out-of-date archives.
-    Args:
-        number (int): The number of archives to keep.
-    If number is 0 or 1, keeps only the most recent archive. If
-    number is 2, keeps the most and second-most recent archives,
-    etc.
-    """
-    number = 1 if int(number) == 0 else int(number)
-
-    archives = sorted(os.listdir("versions"))
-    [archives.pop() for i in range(number)]
-    with lcd("versions"):
-        [local("rm ./{}".format(a)) for a in archives]
-
-    with cd("/data/web_static/releases"):
-        archives = run("ls -tr").split()
-        archives = [a for a in archives if "web_static_" in a]
-        [archives.pop() for i in range(number)]
-        [run("rm -rf ./{}".format(a)) for a in archives]
+def do_deploy(archive_path):
+    """Distributes an archive to your web servers"""
+    if exists(archive_path):
+        try:
+            put(archive_path, "/tmp/")
+            file_name = archive_path.split("/")[-1]
+            no_ext = file_name.split(".")[0]
+            run("mkdir -p /data/web_static/releases/{}/".format(no_ext))
+            run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+                format(file_name, no_ext))
+            run("rm /tmp/{}".format(file_name))
+            run("mv /data/web_static/releases/{}/web_static/* \
+                /data/web_static/releases/{}/".format(no_ext, no_ext))
+            run("rm -rf /data/web_static/releases/{}/web_static".
+                format(no_ext))
+            run("rm -rf /data/web_static/current")
+            run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+                format(no_ext))
+            return True
+        except Exception as e:
+            print("Exception occurred: {}".format(e))
+    return False
