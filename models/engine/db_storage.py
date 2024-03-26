a60d901c773f3e19d7ca63afa9fc7843270b64c5
paschalugwu <ugwupaschal@gmail.com>
2024-03-26 15:08:53 +0100

    chore(engine): improve engines
    Enhanced the engine components by updating FileStorage and DBStorage classes, as well as adding a method to State class for retrieving City objects.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -82,3 +82,7 @@ class DBStorage:
     def close(self):
         """Close the working SQLAlchemy session."""
         self.__session.close()
+
+    def close(self):
+        """Close the working SQLAlchemy session."""
+        self.__session.close()
