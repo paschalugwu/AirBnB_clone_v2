71931d00cdb1b024bc4d9183ce38c1aa8be35e10
paschalugwu <ugwupaschal@gmail.com>
2024-03-26 11:01:06 +0100

    feat(app): initialize Flask web application
        Created a basic Flask web application.
        Defined a route to display 'Hello HBNB!' when accessing the root URL.

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,20 +1,15 @@
 #!/usr/bin/python3
-"""
-Starts a Flask web application.
-"""
+"""Starts a Flask web application"""
 from flask import Flask
 
-# Create an instance of the Flask app
 app = Flask(__name__)
 
-# Defining the Route
-@app.route('/', strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello_hbnb():
-    """
-    Displays 'Hello HBNB!' whe accessing the root URL.
-    """
+    """Displays 'Hello HBNB!'"""
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0")
